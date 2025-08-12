e1bd49df72d05a3916ff46b363cbe9609660ac54
Matej Sliacky <matej.sliacky@protonmail.com>
2025-08-12 21:22:13 +0200

    Added for loops within a booking logic

--- main.go
+++ main.go
@@ -2,7 +2,10 @@
 // package - collection of a source files
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Main function is the entry point of our application
 func main() {
@@ -11,35 +14,48 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets = 50
 	var remainingTickets uint = 50 // uint stands for just positive whole numbers
+	var bookings []string
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-	var bookings []string
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Print("Please enter your first name: ")
-	fmt.Scan(&firstName)
+		fmt.Print("Please enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Print("Please enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Print("Please enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Print("Please enter your email: ")
-	fmt.Scan(&email)
+		fmt.Print("Please enter your email: ")
+		fmt.Scan(&email)
 
-	fmt.Print("Enter the number of tickets you want to purchase: ")
-	fmt.Scan(&userTickets)
+		fmt.Print("Enter the number of tickets you want to purchase: ")
+		fmt.Scan(&userTickets)
 
-	// Update the remaining tickets variable
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName+" "+lastName)
+		// Update the remaining tickets variable
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("Hello %v %v, thank you for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		fmt.Printf("Hello %v %v, thank you for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-	fmt.Printf("These are all our bookings: %v\n", bookings)
+		firstnames := []string{}
+		// _ (Blank identifier) => to ignore variable we don't want to use
+		// 						=> in Go we need to make unused variables explicit
+		for _, booking := range bookings {
+			// strings.FIelds() => splits the string with white space as separator
+			// 					=> returns a slice with the split elements
+			var names = strings.Fields(booking)
+			firstnames = append(firstnames, names[0])
+		}
+
+		fmt.Printf("The first name of bookings are: %v\n", firstnames)
+	}
+
 }
